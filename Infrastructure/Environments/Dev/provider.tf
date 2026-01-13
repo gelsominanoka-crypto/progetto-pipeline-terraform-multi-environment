aa1298d2db8412d1bfe38995a935060024d4dd16
g3lsi <gelsominanoka@gmail.com>
2026-01-13 15:20:36 +0100

    chore: attivato backend remoto S3 e DynamoDB

--- Infrastructure/Environments/Dev/provider.tf
+++ Infrastructure/Environments/Dev/provider.tf
@@ -1,4 +1,3 @@
-# Provider AWS Configuration
 terraform {
   required_version = ">= 1.0"
   
@@ -8,10 +7,18 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  # --- BACKEND SU S3 (La memoria remota) ---
+  backend "s3" {
+    bucket         = "us-east-1-terraform-progetto-prova"
+    key            = "dev/terraform.tfstate"           # <--- Nota: cartella "dev"
+    region         = "us-east-1"                        # La regione del BUCKET
+    dynamodb_table = "terraform-prova-pipeline"
+    encrypt        = true
+  }
 }
 
-# Configure the AWS Provider
+# (Il resto del provider aws rimane uguale)
 provider "aws" {
   region = var.aws_region
 }
-
--- Infrastructure/Environments/Prod/provider.tf
+++ Infrastructure/Environments/Prod/provider.tf
@@ -1,4 +1,3 @@
-# Provider AWS Configuration
 terraform {
   required_version = ">= 1.0"
   
@@ -8,10 +7,17 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  # --- BACKEND SU S3 ---
+  backend "s3" {
+    bucket         = "us-east-1-terraform-progetto-prova"
+    key            = "prod/terraform.tfstate"           # <--- Nota: cartella "prod"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-prova-pipeline"
+    encrypt        = true
+  }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
 }
-
