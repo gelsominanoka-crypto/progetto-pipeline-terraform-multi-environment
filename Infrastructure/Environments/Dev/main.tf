198ba0d4c866c0b248bd00921d62e9cc575fcd3f
g3lsi <gelsominanoka@gmail.com>
2026-01-14 16:42:59 +0100

    deploy prod

--- Infrastructure/Environments/Dev/main.tf
+++ Infrastructure/Environments/Dev/main.tf
@@ -97,7 +97,6 @@ resource "aws_security_group" "ec2" {
 resource "aws_instance" "main" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
-  key_name               = var.key_pair_name
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ec2.id]
 
